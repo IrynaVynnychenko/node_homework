6d7540a6c35d862c7e72d31dd38d4bd6d2c1b94a
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-07-22 18:33:37 +0300

    push

--- httpServer.js
+++ httpServer.js
@@ -6,24 +6,32 @@ var mainCss;
 var blog;
 var about;
 var page404;
+var mainJS;
 
 const server = http.createServer((req, res) => {
 
-    fs.readFile('./folder/index.html', 'utf-8', function(err, pizdec) {
+    fs.readFile('./folder/index.html', 'utf-8', function(err, data) {
         if (err) throw err;
-        htmlFile = pizdec;
+        htmlFile = data;
     });
     fs.readFile('./folder/about.html', 'utf-8', function(err, data) {
+        if (err) throw err;
         about = data;
     });
     fs.readFile('./folder/blog.html', 'utf-8', function(err, data) {
         if (err) throw err;
         blog = data;
     });
-    fs.readFile('./folder/main.css', 'utf-8', function(err, neponyatno) {
-        mainCss = neponyatno;
+    fs.readFile('./folder/main.css', 'utf-8', function(err, data) {
+        if (err) throw err;
+        mainCss = data;
     });
+    fs.readFile('./folder/main.js', 'utf-8', function(err, data) {
+        if (err) throw err;
+        mainJS = data;
+    })
     fs.readFile('./folder/page404.html', 'utf-8', function(err, data) {
+        if (err) throw err;
         page404 = data;
     });
 
@@ -31,6 +39,10 @@ const server = http.createServer((req, res) => {
         case '/main.css':
             res.writeHead(200, { "Content-Type": "text/css" });
             res.end(mainCss);
+            break;
+        case '/main.js':
+            res.writeHead(200, { "Content-Type": "text/javascript" });
+            res.end(mainJS);
             break;
         case '/blog.html':
             res.writeHead(200, { "Content-Type": "text/html" });
@@ -45,9 +57,8 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(htmlFile);
         default:
-            res.writeHead(200, { "Content-Type": "text/html" });
+            res.writeHead(404, { "Content-Type": "text/html" });
             res.end(page404);
-
     }
     res.end();
 })
